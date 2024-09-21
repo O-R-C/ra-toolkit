5a273091e09bc899edb5ba2efe18d6548493ed9d
Andrey Semenov <semenov.orc@gmail.com>
2024-09-21 17:21:59 +0300

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+      generateScopedName: '[local]__[hash:base64:5]',
+    },
+  },
 })
